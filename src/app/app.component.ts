6c06fadbdf9891081c913dcbb4a587aa32e18fc0
u4bi <u4bi@u4bi.com>
2017-05-26 00:51:09 +0900

    three.js import console test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as THREE from 'three';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor(){
+    console.log(THREE);
+
+  }
 }
