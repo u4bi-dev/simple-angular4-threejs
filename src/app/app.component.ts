dfb5c0952e69036ca5a1919ee1dbc819ae23738d
u4bi <u4bi@u4bi.com>
2017-05-26 01:47:02 +0900

    webgl screen initialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,10 @@ export class AppComponent implements OnInit{
   @ViewChild('container') elementRef: ElementRef;
   private container : HTMLElement;
 
+  private scene: THREE.Scene;
+  private camera: THREE.PerspectiveCamera;
+  private renderer: THREE.WebGLRenderer;
+
   constructor(){
     console.log(THREE);
 
@@ -21,5 +25,28 @@ export class AppComponent implements OnInit{
     
     console.log(this.container);
 
+    this.init();
   }
+
+  init(){
+    let screen = {
+      width  : 400,
+      height : 300
+    },
+    view = {
+      angle  : 45,
+      aspect : screen.width / screen.height,
+      near   : 0.1,
+      far    : 1000
+    };
+
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(view.angle, view.aspect, view. near, view.far);
+    this.renderer = new THREE.WebGLRenderer();
+
+    this.renderer.setSize(screen.width, screen.height);
+    this.container.appendChild(this.renderer.domElement);
+
+  }
+
 }
