a610de30225334dada38db982cf5cd400647a0f6
Yu Myungjae <u4bi@letsee.io>
2017-05-26 18:59:57 +0900

    cube render

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,8 @@ export class AppComponent implements OnInit{
   private camera: THREE.PerspectiveCamera;
   private renderer: THREE.WebGLRenderer;
 
+  private cube : THREE.Mesh;
+
   constructor(){
     console.log(THREE);
 
@@ -44,9 +46,24 @@ export class AppComponent implements OnInit{
     this.camera = new THREE.PerspectiveCamera(view.angle, view.aspect, view. near, view.far);
     this.renderer = new THREE.WebGLRenderer();
 
+    this.scene.add(this.camera);
+    this.scene.add(new THREE.AxisHelper(20));
+    
+    this.camera.position.set(10,10,10);
+    this.camera.lookAt(new THREE.Vector3(0,0,0));
+
     this.renderer.setSize(screen.width, screen.height);
     this.container.appendChild(this.renderer.domElement);
 
+
+    let geometry = new THREE.BoxGeometry(5, 5, 5),
+        material = new THREE.MeshBasicMaterial({ color : 0xFFFFFF, wireframe: true });
+        
+    this.cube = new THREE.Mesh( geometry, material );
+    this.cube.position.set(-50,-50,-50);
+    
+    this.scene.add(this.cube);
+    
     this.render();
   }
 
@@ -64,7 +81,10 @@ export class AppComponent implements OnInit{
   }
 
   animate(){
-    console.log('animate');
+    this.cube.rotateX(0.1);
+    this.cube.rotateY(0.1);
+    this.cube.position.addScalar(0.2);
+
   }
 
 }
